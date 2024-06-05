58953a71dddbda926fa1485e24357946a89a7cea
Martin <martin_dotto@hotmail.com>
2024-06-05 18:20:35 -0300

    Fixed Audio Component 6

--- src/components/Audio/Audio.jsx
+++ src/components/Audio/Audio.jsx
@@ -1,29 +1,11 @@
 import { useState, useEffect, useRef } from 'react';
 import './Audio.css';
 import audioIcon from '../../assets/audio2.png';
-import audioFile from '../../audio/remember.mp3';
 
 const Audio = () => {
-  const [isPlaying, setIsPlaying] = useState(true);
   const [isVisible, setIsVisible] = useState(false);
   const [volume, setVolume] = useState(1);
   const audioRef = useRef(null);
-
-  useEffect(() => {
-    // Intentar reproducir automáticamente al cargar
-    const playAudio = async () => {
-      if (audioRef.current) {
-        try {
-          await audioRef.current.play();
-          setIsPlaying(true);
-        } catch (error) {
-          console.error("Error al intentar reproducir automáticamente:", error);
-          setIsPlaying(false); // Ajustar según si se puede reproducir o no
-        }
-      }
-    };
-    playAudio();
-  }, []);
 
   useEffect(() => {
     // Ajustar volumen siempre que cambie
@@ -31,18 +13,6 @@ const Audio = () => {
       audioRef.current.volume = volume;
     }
   }, [volume]);
-
-  const togglePlay = () => {
-    if (audioRef.current) {
-      if (audioRef.current.paused) {
-        audioRef.current.play();
-        setIsPlaying(true);
-      } else {
-        audioRef.current.pause();
-        setIsPlaying(false);
-      }
-    }
-  };
 
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
@@ -54,7 +24,6 @@ const Audio = () => {
 
   return (
     <div className="audio-player">
-      <audio ref={audioRef} src={audioFile} loop type="audio/mp3"></audio>
       <img
         src={audioIcon}
         alt="Toggle Player"
@@ -63,9 +32,6 @@ const Audio = () => {
       />
       {isVisible && (
         <div className="controls">
-          <button className="play-pause-button" onClick={togglePlay}>
-            {isPlaying ? <div className="pause-icon"></div> : <div className="play-icon"></div>}
-          </button>
           <input
             type="range"
             min="0"
