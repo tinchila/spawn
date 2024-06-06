3171c146905ba5ae5c8d04114d538d81264ad5e9
Martin <martin_dotto@hotmail.com>
2024-06-05 21:26:02 -0300

    Fixed NavBar 10

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,18 +4,14 @@ import logo from '../../assets/logoweb.png';
 import { Link, animateScroll as scroll } from 'react-scroll';
 import menu from '../../assets/menu.png';
 
-const createParticles = (containerId, num) => {
-  const container = document.getElementById(containerId);
-  if (container) {
-    for (let i = 0; i < num; i++) {
-      let particle = document.createElement("div");
-      particle.className = "particle";
-      particle.style.left = `${Math.random() * 100}%`;
-      particle.style.animationDelay = `${Math.random()}s`;
-      container.appendChild(particle);
-    }
-  } else {
-    console.log('Container not found:', containerId);
+// Función para crear las partículas de smog
+const createParticles = (container, num) => {
+  for (let i = 0; i < num; i++) {
+    let particle = document.createElement("div");
+    particle.className = "particle";
+    particle.style.left = `${Math.random() * 100}%`;
+    particle.style.animationDelay = `${Math.random()}s`;
+    container.appendChild(particle);
   }
 };
 
@@ -23,9 +19,11 @@ const Navbar = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
-    createParticles("smog-container", 20);
+    const smogContainer = document.getElementById("smog-container");
+    createParticles(smogContainer, 20);
   }, []);
 
+  // Maneja el clic en el logo para asegurar que siempre lleve a la parte superior
   const handleLogoClick = () => {
     scroll.scrollToTop({
       duration: 500,
@@ -39,6 +37,7 @@ const Navbar = () => {
         <img src={logo} alt='Logo' className='logo' />
       </div>
       <div className='desktopMenu'>
+        {/* Otros enlaces siguen utilizando react-scroll Link */}
         <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
         <Link activeClass='active' to='about' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Bio</Link>
         <Link activeClass='active' to='events' spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Events</Link>
